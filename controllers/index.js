13a82dbc694f802c06f459bfa8955eead656b993
Joshua Wang <joshuawang3110@gmail.com>
2018-04-07 19:52:10 -0400

    Add mysql

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,23 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express'),
+      router = express.Router();
+      mysql = require('mysql2');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+// create connection
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Mysql connected...');
+});
 
 router.get('/', function(req, res) {
   res.render('main');
