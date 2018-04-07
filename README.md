b5016c44b365a02de201945929bae8e4eb31ff6a
Joshua Wang <joshuawang3110@gmail.com>
2018-04-07 19:49:13 -0400

    Init project with jQuery and semantic-ui

---
+++ README.md
@@ -0,0 +1,6 @@
+# API web application for Invisible Cities
+
+Invisible Cities is a mobile dashboard for collaboratively developing AR monuments and expansive artworks in public squares.
+
+## APIs
+
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+      bodyParser = require('body-parser'),
+      app = express(),
+      port = process.env.PORT || 3000;
+
+app.use(require('./controllers'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.listen(port, function () {
+  console.log('listening on http://localhost:' + port);
+});
